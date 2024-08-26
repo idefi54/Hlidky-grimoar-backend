61bccdb4a3430b46850b140855767a83558cdb4b
idefixCZ <59373818+idefi54@users.noreply.github.com>
2024-08-26 16:32:38 +0200

    gfzuj

---
+++ GrimoarBackend/DTOs/Enums.cs
@@ -0,0 +1,13 @@
+﻿namespace GrimoarBackend.DTOs
+{
+
+    public enum StranaEnum { SVETLO, TMA};
+    public enum FrakceEnum { ND, DH, INKVIZICE, CIVIL};
+    public enum SpecializaceEnum { SVETLY_MAG, OBRATEN, ZARIKAVAC, LECITEL, JASNOVIDEC, TEMNY_MAG, VLKODLAK, VEDMA, UPIR, VYKLADAC};
+    public enum ZivelEnum { OHEN, VODA, ZEME, VZDUCH, MYSL, SMRT, NEZARAZENA};
+    public enum StupenEnum { AAM, UM, TRIM, ARCH, MEGA};
+    public enum StavKouzla { ZAZADANE, SCHVALENE };
+    public enum StavReportuEnum { ZALOZENY, ROZPRACOVANY, ODESLANY, SCHVALENY};
+    public enum KodChybyEnum { OBECNA_CHYBA, CHYBA_VSTUPU, NENALEZENO};
+
+}
---
+++ GrimoarBackend/DTOs/PostavaZakladDto.cs
@@ -0,0 +1,23 @@
+﻿namespace GrimoarBackend.DTOs
+{
+    public record class PostavaZakladDto
+    {
+        public long id;
+        public string jmeno;
+        public string prijmeni;
+        public string prezdivka;
+        public string sereJmeno;
+        public StranaEnum strana;
+        public SpecializaceEnum specializace;
+        public FrakceEnum frakce;
+        public long kategorie;
+        public long strop;
+        public bool stropDosazen;
+        public long pocetExpuCelkem;
+        public long pocetVolnychExpu;
+        public string kdoJeKdo;
+        public string povidaSe;
+        public string biografie;
+
+    }
+}
